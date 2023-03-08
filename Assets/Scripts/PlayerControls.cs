1d98d335a116d1362f2b45a535e1227accd3d15a
dimmmark <105634628+dimmmark@users.noreply.github.com>
2023-03-08 13:08:50 +0400

    Add jump

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
@@ -19,11 +20,22 @@ public class PlayerControls : MonoBehaviour
         {
             _rigitbody2D.velocity = Vector2.up * _jumpSpeed;
         }
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        var mousePosX = new Vector2(mousePos.x, transform.position.y);
-        transform.position = Vector2.Lerp(transform.position, mousePosX, _speed);
+       
+       
     }
-    
+    private void FixedUpdate()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePosX = new Vector2(mousePos.x, transform.position.y);
+        //transform.position = Vector2.Lerp(transform.position, mousePosX, _speed);
+        Vector2 direction = (mousePosX - (Vector2)transform.position);
+        var directionClamped =(Mathf.Clamp(direction.x, -3, 3));
+        _rigitbody2D.AddForce(Vector2.right * directionClamped * _speed);
+       // _rigitbody2D.MovePosition((Vector2)transform.position + direction * _speed * Time.deltaTime);
+        Debug.Log(directionClamped);
+    }
+
+
     private void OnCollisionStay2D(Collision2D collision)
     {
         _isGrounded = true;
